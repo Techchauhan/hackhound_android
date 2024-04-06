c1221ead61f6cbba6ee507ca4d006b5df4eaff99
Techchauhan <rishabkumarchauhan@gmail.com>
2024-04-06 08:28:27 +0530

    change occurs

--- lib/screen/splashScreen/splashScreen.dart
+++ lib/screen/splashScreen/splashScreen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
       FirebaseAuth.instance.authStateChanges().listen((User? user) {
         if (user == null) {
           // If no user is logged in, navigate to the login page
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>OnboardingScreen()
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>StartingHomePage()
           ));
         } else {
           // If a user is logged in, navigate to the home page
